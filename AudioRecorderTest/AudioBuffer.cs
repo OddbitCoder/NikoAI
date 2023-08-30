39582c00e42738b6999b6390b0cc7b14a013b489
Oddbit Coder <oddbitcoder@gmail.com>
2023-08-30 11:54:20 +0200

    Adds AudioBuffer

---
+++ AudioRecorderTest/AudioBuffer.cs
@@ -0,0 +1,66 @@
+﻿using NAudio.Wave;
+
+namespace OddbitAi.AudioRecorder
+{
+    internal class AudioBuffer
+    {
+        private int bufferSize; // number of bytes
+
+        private readonly Queue<byte> rawData
+            = new();
+
+        public byte[] RawData 
+            => rawData.ToArray();
+        public int RawByteCount
+            => rawData.Count;
+
+        public void Clear()
+        {
+            rawData.Clear();
+        }
+
+        public AudioBuffer(int bufferSize)
+        {
+            this.bufferSize = bufferSize;
+        }
+
+        public void WriteData(byte[] buffer, int bufferLen)
+        {
+            for (int i = 0; i < bufferLen; i++) 
+            {
+                rawData.Enqueue(buffer[i]);
+                while (rawData.Count > bufferSize) 
+                {
+                    rawData.Dequeue();
+                }
+            }
+        }
+
+        public void WriteToFile(string fileName, WaveFormat wavFormat)
+        {
+            //Console.WriteLine(fileName);
+            //Console.WriteLine(RawByteCount);
+            using (WaveFileWriter writer = new WaveFileWriter(fileName, wavFormat))
+            {
+                var rawData = RawData; // snapshot
+                writer.Write(rawData, 0, rawData.Length);
+                writer.Flush();
+            }
+        }
+
+        public byte[] GetWavBytes(WaveFormat wavFormat)
+        {
+            var ms = new MemoryStream();
+            using (WaveFileWriter writer = new WaveFileWriter(ms, wavFormat))
+            {
+                var rawData = RawData; // snapshot
+                writer.Write(rawData, 0, rawData.Length);
+                writer.Flush();
+                var msBuffer = ms.GetBuffer();
+                var destBuffer = new byte[msBuffer.Length];
+                Array.Copy(msBuffer, destBuffer, msBuffer.Length);
+                return destBuffer;
+            }
+        }
+    }
+}
--- AudioRecorderTest/AudioRecorderMainForm.cs
+++ AudioRecorderTest/AudioRecorderMainForm.cs
@@ -1,29 +1,50 @@
 using NAudio.Wave;
+using System.Runtime.InteropServices;
+
+// https://stackoverflow.com/questions/11500222/how-to-write-naudio-wavestream-to-a-memory-stream
+// https://stackoverflow.com/questions/9804519/waveinevent-sample-event-frequency
 
 namespace OddbitAi.AudioRecorder
 {
     public partial class AudioRecorderMainForm : Form
     {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
         public AudioRecorderMainForm()
         {
             InitializeComponent();
         }
 
-        WaveFileWriter writer = null;
-        WaveInEvent waveIn = new();
+        //WaveFileWriter writer = null;
+        AudioBuffer buffer = new(160000); // 10 seconds
+        WaveInEvent waveIn = new() {
+            BufferMilliseconds = 250,
+            NumberOfBuffers = 2,
+            //DeviceNumber = 0,
+            WaveFormat = new WaveFormat(8000, 16, 1)
+        };
         bool closing = false;
         string outputFilePath;
 
         private void AudioRecorderMainForm_Load(object sender, EventArgs e)
         {
+            AllocConsole();
+
             var outputFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "NAudio");
             Directory.CreateDirectory(outputFolder);
             outputFilePath = Path.Combine(outputFolder, "recorded.wav");
 
             waveIn.DataAvailable += (s, a) =>
             {
-                writer.Write(a.Buffer, 0, a.BytesRecorded);
-                if (writer.Position > waveIn.WaveFormat.AverageBytesPerSecond * 30)
+                //writer.Write(a.Buffer, 0, a.BytesRecorded);
+                //if (writer.Position > waveIn.WaveFormat.AverageBytesPerSecond * 30)
+                //{
+                //    waveIn.StopRecording();
+                //}
+                buffer.WriteData(a.Buffer, a.BytesRecorded);
+                if (buffer.RawByteCount == 160000) 
                 {
                     waveIn.StopRecording();
                 }
@@ -31,8 +52,10 @@ namespace OddbitAi.AudioRecorder
 
             waveIn.RecordingStopped += (s, a) =>
             {
-                writer?.Dispose();
-                writer = null;
+                //writer?.Dispose();
+                //writer = null;
+                //buffer.WriteToFile(outputFilePath, waveIn.WaveFormat);
+                File.WriteAllBytes(outputFilePath, buffer.GetWavBytes(waveIn.WaveFormat));
                 buttonRecord.Enabled = true;
                 buttonStop.Enabled = false;
                 if (closing)
@@ -44,7 +67,7 @@ namespace OddbitAi.AudioRecorder
 
         private void buttonRecord_Click(object sender, EventArgs e)
         {
-            writer = new WaveFileWriter(outputFilePath, waveIn.WaveFormat);
+            //writer = new WaveFileWriter(outputFilePath, waveIn.WaveFormat);
             waveIn.StartRecording();
             buttonRecord.Enabled = false;
             buttonStop.Enabled = true;
