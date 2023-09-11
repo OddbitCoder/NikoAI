cc1fe5e5063547c6a01cce1fdd08545fe56c8a33
Oddbit Coder <oddbitcoder@gmail.com>
2023-09-11 12:55:06 +0200

    Copies inference program to /files/inf

--- YOLOv7/files/run.sh
+++ YOLOv7/files/run.sh
@@ -1,1 +1,1 @@
-python3 detect.py --weights /models/yolov7.pt --no-trace --conf 0.25 --img-size 640 --source /files/in/horses.jpg --project /files/out --name "" --exist-ok
+python3 /files/inf/detect.py --weights /models/yolov7.pt --no-trace --conf 0.25 --img-size 640 --source /files/in/horses.jpg --project /files/out --name "" --exist-ok
